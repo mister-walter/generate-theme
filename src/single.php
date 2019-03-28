33f953367339bd4e33ac7dfa4835099162d34f44
Joseph Wang <wang.jo@husky.neu.edu>
2019-03-28 00:45:18 -0400

    form style scope bump, position details page

--- src/single.php
+++ src/single.php
@@ -12,10 +12,12 @@
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
-$context['comment_form'] = TimberHelper::get_comment_form();
 
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
+}
+else if (in_category('position')) {
+	Timber::render( 'position.twig', $context );
 } else {
 	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
 }
