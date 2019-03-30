527426b6bea03643bd149f3da9d183b3a123a61e
Joseph Wang <wang.jo@husky.neu.edu>
2019-03-29 23:27:45 -0400

    style updates across team, apply, home - js scrollTo for buttons

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,24 @@
 // A simple wrapper for all your custom jQuery;
 // everything in this file will be run on every page
 
-$(function() {});
+$(function() {
+  // Homepage: scroll to about from hero
+  $('#hero .cta .btn').click(function() {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $('#about').offset().top - 80,
+      },
+      800
+    );
+  });
+
+  // Application page: scroll to application list
+  $('#apply-hero .cta .btn').click(function() {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $('.apply--applications--section').offset().top - 80,
+      },
+      800
+    );
+  });
+});
